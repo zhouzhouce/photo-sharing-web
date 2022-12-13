00ad4544040753db8734e85fccdd06458e0431ca
zhouzhouce <91292249+zhouzhouce@users.noreply.github.com>
2022-12-12 21:43:35 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -53,7 +53,7 @@ function App({ Component, pageProps }) {
     return (
         <>
             <Head>
-                <title>Next.js 11 - User Registration and Login Example</title>
+                <title>Photo Sharing Web</title>
                 
                 {/* eslint-disable-next-line @next/next/no-css-tags */}
                 <link href="//netdna.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" />
